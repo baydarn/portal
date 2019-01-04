9b9c6361dc772d2c027312d656aa1f0d2cad562e
Neslin <neslin_baydar@hotmail.com>
2019-01-04 14:17:34 +0300

    initial commit

---
+++ console/migrations/m190103_135457_oylama.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190103_135457_oylama
+ */
+class m190103_135457_oylama extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m190103_135457_oylama cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m190103_135457_oylama cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
